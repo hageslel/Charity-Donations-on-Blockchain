f3f6bbf435b1a52c6adf5d0177b3d966162707ba
Peter S. Loft <peterloft3@gmail.com>
2021-02-03 15:03:18 -0800

    Update Charity_designed_contract_v1.sol

--- Smart-Contracts/Charity_designed_contract_v1.sol
+++ Smart-Contracts/Charity_designed_contract_v1.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.5.0;
 
 import "github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/math/SafeMath.sol";
 
-contract Donation_Portal is set_pcts {
+contract Donation_Portal {
   using SafeMath for uint256;
   
   address internal org_admin = msg.sender;
