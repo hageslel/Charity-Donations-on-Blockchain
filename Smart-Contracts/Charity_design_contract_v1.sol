49a08adbe73965464782d0f86a007c8b000e1425
hageslel <hageslel@gmail.com>
2021-02-07 12:17:35 -0800

    adding back smart contracts that were deleted due to merge conflicts, and organizing all contracts into folder

---
+++ Smart-Contracts/Charity_design_contract_v1.sol
@@ -0,0 +1,115 @@
+pragma solidity ^0.5.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/drafts/Counters.sol";
+
+
+contract Donation_Portal {
+    
+  using Counters for Counters.Counter;
+  Counters.Counter donated;
+
+  address internal org_admin = msg.sender;
+  uint public total_donations_received;
+  uint public last_deposit_block;
+  uint public last_deposit_amount;
+  
+  address public last_to_deposit;
+  address payable mission;
+  address payable investment;
+  address payable marketing;
+  address payable operations;
+  address payable taxes;
+  
+  uint bd_1;
+  uint bd_2;
+  uint bd_3;
+  uint bd_4;
+  uint bd_5;
+  
+  uint points;
+  uint amnt_mis;
+  uint amnt_inv;
+  uint amnt_mar;
+  uint amnt_ope;
+  uint amnt_tax;
+  
+  constructor(
+      address payable _mission,
+      address payable _investment,
+      address payable _marketing,
+      address payable _operations,
+      address payable _taxes
+  ) public {
+    mission = _mission;
+    investment = _investment;
+    marketing = _marketing;
+    operations = _operations;
+    taxes = _taxes;
+  }
+  
+
+      struct Data {
+        address donor;
+        uint count;
+    }
+    
+    mapping(uint => Data) data;
+    
+    event Donation(uint donate, string report_uri);
+    
+    function set_pcts(
+        uint _mission_pct, 
+        uint _invest_pct, 
+        uint _market_pct, 
+        uint _operate_pct, 
+        uint _tax_pct
+        ) 
+        public returns (uint, uint, uint, uint, uint) {
+        require(msg.sender == org_admin, "Access Denied, you are not the appropriate owner");
+        
+        bd_1 = _mission_pct; 
+        bd_2 = _invest_pct; 
+        bd_3 = _market_pct; 
+        bd_4 = _operate_pct; 
+        bd_5 = _tax_pct;
+        require(_mission_pct + _invest_pct + _market_pct + _operate_pct + _tax_pct == 100, "The sum of all percentages MUST equal 100 to continue");
+        return (bd_1, bd_2, bd_3, bd_4, bd_5);
+    }
+    
+    function deposit() public payable {
+        
+        total_donations_received = total_donations_received + msg.value;
+        points = msg.value/100;
+        
+        amnt_mis = points * bd_1;
+        amnt_inv = points * bd_2;
+        amnt_mar = points * bd_3;
+        amnt_ope = points * bd_4;
+        amnt_tax = points * bd_5;
+        
+        mission.transfer(amnt_mis);
+        investment.transfer(amnt_inv);
+        marketing.transfer(amnt_mar);
+        operations.transfer(amnt_ope);
+        taxes.transfer(amnt_tax);
+        
+        last_to_deposit = msg.sender;
+        last_deposit_amount = msg.value;
+        last_deposit_block = block.number;
+    }
+    
+    function() external payable {}
+    
+    function registerDonor(address donor) public returns(uint) {
+        donated.increment();
+        uint donate = donated.current();
+        data[donate] = Data(donor, 0);
+        return donate;
+    }
+    function reportDonation(uint donate, string memory report_uri) public returns(uint) {
+        data[donate].count += 1;
+        // Permanently associates the report_uri with the token_id on-chain via Events for a lower gas-cost than storing directly in the contract's storage.
+        emit Donation(donate, report_uri);
+        return data[donate].count;
+    }
+}
---
+++ Smart-Contracts/charity_designed_roughdraft.sol
@@ -0,0 +1,74 @@
+pragma solidity ^0.5.0;
+
+import "github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/math/SafeMath.sol";
+
+// This contract was written to more accurately track the division of donations made to charities in order to help increase transparency and accountability.
+// The main idea is to force specification reasons in withdrawal, requiring boolean true/false flags before withdrawals may be made.
+
+contract Donation_Portal {
+  address payable org_admin;
+  address payable wing_admin;
+  
+  uint public balance;
+  uint public last_withdraw_block;
+  uint public last_withdraw_amount;
+  address public last_to_withdraw;
+
+  constructor(address payable _one, address payable _two) public {
+    org_admin = _one;
+    wing_admin = _two;
+  }
+
+  function withdraw(
+      uint wei_amount,
+      bool mission,
+      bool investment,
+      bool marketing,
+      bool operations,
+      bool taxes
+      ) public {
+    require(msg.sender == org_admin || msg.sender == wing_admin, "You don't own this account!");
+    require(balance >= wei_amount);
+    if (mission == true) {
+        msg.sender.transfer(wei_amount);
+        balance -= wei_amount;
+    }
+    
+    if (investment == true) {
+        msg.sender.transfer(wei_amount);
+        balance -= wei_amount;
+    }
+    
+    if (marketing == true) {
+        msg.sender.transfer(wei_amount);
+        balance -= wei_amount;
+    }
+    
+    if (operations == true) {
+        msg.sender.transfer(wei_amount);
+        balance -= wei_amount;
+    }
+    
+    if (taxes == true) {
+        msg.sender.transfer(wei_amount);
+        balance -= wei_amount;
+    }
+
+    if (last_to_withdraw != msg.sender) {
+      last_to_withdraw = msg.sender;
+    }
+
+    last_withdraw_block = block.number;
+    last_withdraw_amount = wei_amount;
+    
+    
+  }
+
+  function deposit() public payable {
+      balance = address(this).balance;
+  }
+  
+  
+
+  function() external payable {}
+}
